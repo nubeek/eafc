e8946dd603bec87d81dd2b9ec896d8949277ec4a
Philip <philip@Philips-MacBook-Air.local>
2025-05-29 12:07:32 +0200

    Fix entry file, rebuild, deploy

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/eafc/' // 👈 IMPORTANT for GitHub Pages
-});
